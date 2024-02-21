b0bc523f2c7ad05c5b7859f48dc767d4fa02075c
Kv <67178228+kvsoftware@users.noreply.github.com>
2024-02-21 01:07:08 +0000

    Update content

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,9 +4,9 @@ const projects = [
     title: "Air Pollution Checker",
     image: "images/air-pollution-checker.png",
     shortDescription:
-      "Simplest air pollution checker application on the PlayStore.",
+      "Monitor real-time air quality levels by using data from the World Air Quality Index.",
     description:
-      "My “Air Pollution Checker” application is very simple to check the quality of air. Don’t spend much time to learn how to use it. Check out the screenshots and see for yourself. You will not be disappointed!!",
+      "This application is designed for Android devices, providing users with the capability to monitor air quality levels at their current location or any desired place. Utilizing data sourced from the World Air Quality Index (https://waqi.info/), the app offers a reliable and comprehensive overview of air quality parameters. With this app, users gain valuable insights into air pollution levels, enabling them to make informed decisions about their activities and health. Whether planning outdoor events, exercising, or simply concerned about air quality in their vicinity, users can rely on accurate and up-to-date information provided by the World Air Quality Index database.",
     type: "mobile",
     projectType: "personal",
     screenshots: [
@@ -25,8 +25,10 @@ const projects = [
     id: 4,
     title: "Money Tracker",
     image: "images/money-tracker.png",
-    shortDescription: "Simplest money tracker application on the PlayStore.",
-    description: "Simplest money tracker application on the PlayStore.",
+    shortDescription:
+      "Record transactions, track spending behavior, set budgets, and gain insights into their financial health.",
+    description:
+      "This application is designed for Android devices, erves as a comprehensive financial management tool, empowering users to efficiently record their financial transactions and gain valuable insights into their spending behavior. With its intuitive interface and robust features, users can easily track their income, expenses, and savings, thereby enabling better financial planning and decision-making. Through this app, users can conveniently log all their financial activities, including purchases, bill payments, income sources, and more, ensuring a detailed and accurate record of their financial transactions. Additionally, the app offers categorization options, allowing users to classify their expenses into different categories such as accommodation, bill, food, and others, facilitating a clearer understanding of their spending habits.",
     type: "mobile",
     projectType: "personal",
     screenshots: [
@@ -45,8 +47,9 @@ const projects = [
     title: "Simoorgh Tennis",
     image: "images/simoorgh-tennis.png",
     shortDescription:
-      "Tennis booking application of Simoorgh Tennis Academy BKK.",
-    description: "Tennis booking application of Simoorgh Tennis Academy BKK.",
+      "Reserve your spot on the tennis court at Simoorgh Tennis Academy Bangkok.",
+    description:
+      "This application, specifically designed for Android devices, offers users the convenient capability to book tennis sessions at Simoorgh Tennis Academy Bangkok. Whether you're a seasoned player or just starting out, our user-friendly interface allows you to easily browse available time slots, select your preferred court, and secure your reservation with just a few taps. With our intuitive booking system, managing your tennis activities has never been simpler. Experience the ease and efficiency of organizing your tennis sessions with our app, designed to enhance your experience at Simoorgh Tennis Academy Bangkok.",
     type: "mobile",
     projectType: "personal",
     screenshots: [
@@ -65,9 +68,9 @@ const projects = [
     title: "Medicine Alert",
     image: "images/medicine-alert.png",
     shortDescription:
-      "Simplest and most beautiful medicine alert application on the PlayStore.",
+      "Remind the user to take their pill to help them stay on track with their medication schedule.",
     description:
-      "Simplest and most beautiful medicine alert application on the PlayStore.",
+      "This application, specifically tailored for Android devices, offers users a comprehensive pill reminder feature designed to ensure they never miss taking their medication. With this intuitive functionality, users can easily set up personalized reminders for each medication, helping them stay on track with their treatment regimen and promoting better health management. From daily vitamins to prescription medications, this app empowers users to manage their pill schedule efficiently, providing peace of mind and enhancing overall well-being.",
     type: "mobile",
     projectType: "personal",
     screenshots: [
@@ -85,8 +88,10 @@ const projects = [
     id: 1,
     title: "BTS On to go",
     image: "images/bts-on-to-go.png",
-    shortDescription: "Travelling guidance by BTS Skytrain application.",
-    description: "Travelling guidance by BTS Skytrain application.",
+    shortDescription:
+      "Search for nearby attractions near BTS Skytrain stations to enhance the exploration experience in Bangkok.",
+    description:
+      "This Android application offers users the convenient functionality to search for nearby attractions in proximity to BTS Skytrain stations, enhancing their exploration experience. Not only does it provide information on the attractions themselves, but it also guides users on which specific station they should embark and disembark from, streamlining their travel planning process. Additionally, the app presents pricing details and estimated travel times, enabling users to make informed decisions about their itinerary and budget management while exploring the vibrant cityscape of Bangkok.",
     type: "mobile",
     projectType: "personal",
     screenshots: [
